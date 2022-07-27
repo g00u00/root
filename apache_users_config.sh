657f9899212e30173d07d5e23e385b726df127ca
g00u00 <g00u00@outlook.com>
2022-07-27 13:23:07 +0300

    Update apache_users_config.sh

--- apache_users_config.sh
+++ apache_users_config.sh
@@ -21,6 +21,15 @@ apt install zip
 apt install unzip
 apt install vim
 
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+node -v
+npx -v
+
+
 echo ""
 echo "Устанавливаем временной  пояс"
 rm /etc/localtime
